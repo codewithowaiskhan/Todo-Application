65c2d48c3dbdf921208fed3ce02a0dbcc0d56561
codewithowaiskhan <imowaiskhanofficial@gmail.com>
2024-05-20 02:16:31 +0500

    Adding a Todo Application

---
+++ main.ts
@@ -0,0 +1,93 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+
+let todoList: string [] = [];
+let conditions = true;
+
+// Print welcome message
+console.log(chalk.magenta.bold("\n \t Welcome to CodeWithOwaisKhan - Todo-List Application\n"));
+
+let main = async ()=>{
+    while(conditions){
+        let option = await inquirer.prompt([
+            {
+                name: "choice",
+                type: "list",
+                message: "Select an option you want to do:",
+                choices: ["Add Task", "Delete Task", "Update Task", "View Todo-List", "Exit",]
+            }
+        ]);
+        if(option.choice === "Add Task"){
+            await addTask()
+        }
+        else if(option.choice === "Delete Task"){
+            await deleteTask()
+        }
+        else if(option.choice === "Update Task"){
+            await updateTask()
+        }
+        else if(option.choice === "View Todo-List"){
+            await viewTask()
+        }
+        else if (option.choice === "Exit"){
+            conditions = false;
+        }
+    }
+}
+// function to add new task to the list
+let addTask = async ()=>{
+let newTask = await inquirer.prompt([
+    {
+        name: "task",
+        type: "input",
+        message: "Enter Your New Task",
+    }
+    ]);
+    todoList.push(newTask.task);
+    console.log(`\n ${newTask.task} Task added sucessfully in Todo-List\n`);
+}
+
+// function to view all Todo-list Task
+let viewTask =() =>{
+    console.log("\n Your Todo-List:\n");
+    todoList.forEach((task, index) => {
+        console.log(`${index + 1}: ${task}`)
+    });
+    console.log("\n");
+    }
+    // function to delete a task from the list
+    let deleteTask = async () =>{
+        await viewTask()
+        let taskindex = await inquirer.prompt([
+            {
+                name: "index",
+                type: "number",
+                message: "Enter the `index no.` of the task you want to delete",
+            }
+        ]);
+        let deletedTask = todoList.splice(taskindex.index - 1, 1);
+        console.log(`\n ${deletedTask} this task has been deleted sucessfully from your Todo-List\n`);
+    }
+    // function to update a task
+    let updateTask = async () => {
+        await viewTask()
+        let update_task_index = await inquirer.prompt([
+            {
+                name: "index",
+                type: "number",
+                message: "Enter the `index no` of the task you want to update",
+            },
+            {
+                name: "new_task",
+                type: "input",
+                message: "Now Enter new task name :",
+            }
+        ]);
+        todoList[update_task_index.index - 1] = update_task_index.new_task
+        console.log(`\n Task at index no. ${update_task_index.index - 1} updated sucessfully [For updated list check option: "View Todo-List"]\n`)
+    }
+
+    main();
